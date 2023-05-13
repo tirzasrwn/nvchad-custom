1dd2e181f66b887ab484ba873ed560a680ea79ba
tirzasrwn <tirzasrwn@gmail.com>
2023-05-13 13:58:42 +0700

    Initial commit

---
+++ mappings.lua
@@ -0,0 +1,34 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+  },
+}
+
+-- more keybinds!
+
+-- M.dap = {
+--   n = {
+--     ["<leader>rb"] = {
+--       "<cmd> DapToggleBreakpoint <CR>",
+--       "Toggle breakpoint",
+--     },
+--   }
+-- }
+
+-- Bind for lsp tool
+--   M.lsp = {
+--   n = {
+--     ["<leader>lf"] = {
+--       function ()
+--         local lspbuf = require('vim.lsp.buf');
+--         lspbuf.format();
+--       end,
+--       "format using lsp",
+--     }
+--   }
+-- }
+
+return M
