a998ee1f753ee67fbc583ddb87ad6b998917e71b
tirzasrwn <tirzasrwn@gmail.com>
2023-09-08 07:56:47 +0700

    feat: Change theme to catppuccin and set statusline separator to block.

--- chadrc.lua
+++ chadrc.lua
@@ -5,11 +5,15 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "chadracula",
-  theme_toggle = { "chadracula", "one_light" },
+  theme = "catppuccin",
+  theme_toggle = { "catppuccin", "one_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
+  statusline = {
+    theme = "default",
+    separator_style = "block",
+  },
 }
 
 M.plugins = "custom.plugins"
