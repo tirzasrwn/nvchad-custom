38d291af7f70923b2b8323351812173d2ad8919c
tirzasrwn <tirzasrwn@gmail.com>
2024-01-10 10:09:36 +0700

    feat: Add custom mappings.

--- mappings.lua
+++ mappings.lua
@@ -37,4 +37,12 @@ M.lsp = {
   },
 }
 
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzz")
+vim.keymap.set("n", "N", "Nzz")
+vim.keymap.set("n", "{", "{zz")
+vim.keymap.set("n", "}", "}zz")
+vim.keymap.set("n", "]]", "]]zz")
+
 return M
